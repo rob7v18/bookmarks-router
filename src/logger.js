8ca4dc588b908a6967ca2d32f6dc5cfbfc76d17b
Robert Vang <rob7v18@gmail.com>
2020-10-22 21:28:54 -0500

    second commit

--- src/logger.js
+++ src/logger.js
@@ -1,6 +1,6 @@
 const winston = require('winston')
 const { NODE_ENV } = require('./config')
-
+ 
 const logger = winston.createLogger({
   level: 'info',
   format: winston.format.json(),
@@ -8,11 +8,11 @@ const logger = winston.createLogger({
     new winston.transports.File({ filename: 'info.log' })
   ]
 })
-
+ 
 if (NODE_ENV !== 'production') {
   logger.add(new winston.transports.Console({
     format: winston.format.simple()
   }))
 }
-
-module.exports = loggerpom
+ 
+module.exports = logger
